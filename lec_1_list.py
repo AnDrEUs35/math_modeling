327b0507f91cb513fc482c5514353bb425a2e0db
AnDrEUs35 <146358937+AnDrEUs35@users.noreply.github.com>
2023-10-05 16:19:59 +0000

    добавил файлы

---
+++ lec_1_list.py
@@ -0,0 +1,19 @@
+a = [1, 3, 6] # create list
+print(a[0])  # вызов 0 элемента списка
+
+b = [8, 10, 11]
+
+c = a + b
+print(c)
+
+#c = a - b
+
+print(c * 2)
+
+c.append('Good') # добавление элемента / списка
+print(c)
+
+a.append(b)
+print(a)
+
+print(len(a))
