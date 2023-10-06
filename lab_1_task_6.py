2f946dd1fb921032fa0d16830d3f5980d47b3cb8
AnDrEUs35 <146358937+AnDrEUs35@users.noreply.github.com>
2023-10-06 13:29:21 +0000

    сделал лабу

---
+++ lab_1_task_6.py
@@ -0,0 +1,14 @@
+info = []
+
+info.append(str(input("Возраст:")))
+info.append(str(input("Пол:")))
+info.append(str(input("Имя:")))
+info.append(str(input("Город:")))
+info.append(str(input("В каком классе вы учитесь:")))
+
+print(info)
+
+
+
+
+
