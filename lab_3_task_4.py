8b1ff6d531d24e2f9d336a996e6509eaf065eb4b
AnDrEUs35 <146358937+AnDrEUs35@users.noreply.github.com>
2023-11-03 07:06:47 +0000

    gjfkf

--- lab_3_task_4.py
+++ lab_3_task_4.py
@@ -1,26 +1,19 @@
 import numpy as np
-#A[i, j] = sin(N · (i+1) + M · (j + 1)) 
+#A[i, j] = sin(N · i + M · j) 
 M = int(input('введите ')) 
 N = int(input('введите '))
 NxM = np.zeros((N, M))
-a = -1
+
 
 for i in np.arange(N):
-    a += 1
-    b = -1
     for j in np.arange(M):
-        b += 1
-        NxM[a, b] = np.sin(N * (a + 1) + M * (b + 1))
+        NxM[i, j] = np.sin(N * (i + 1) + M * (j + 1))
 
 
-a = -1
 for i in range(N):
-    a += 1
-    b = -1
-    for i in range(M):
-        b += 1
-        if NxM[a, b] < 0:
-            NxM[a, b] = 0
-        elif NxM[a, b] >= 0:
+    for j in range(M):
+        if NxM[i, j] < 0:
+            NxM[i, j] = 0
+        elif NxM[i, j] >= 0:
             NxM = NxM
 print(NxM)
