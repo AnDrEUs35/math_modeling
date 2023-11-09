1f0f2019cf2a47d5253ff886bed49184c94fd80c
AnDrEUs35 <146358937+AnDrEUs35@users.noreply.github.com>
2023-11-09 14:50:48 +0000

    jhlkzxvb

--- lab_3_task_5.py
+++ lab_3_task_5.py
@@ -1,24 +1,23 @@
 import numpy as np
 from lab_3_task_4 import NxM, M, N
+x = int(input('введите номер столбца'))
+y = int(input('введите номер столбца'))
 
-slice1 = NxM[::, 0]
+slice1 = NxM[::, x]
+slice2 = NxM[::, y]
+print(slice1)
+print(slice2)
 
-slice2 = NxM[::, 1]
 c = []
 d = []
 for i in range(0, len(slice1)):
     c.append(slice1[i])
-    
-
-for i in range(0, len(slice2)):
     d.append(slice2[i])
     
 slice1 = np.array(d)
 slice2 = np.array(c)
 
 for i in range(len(slice1)):
-    NxM[i,0] = slice1[i]
-
-for i in range(len(slice2)):
-    NxM[i, 1] = slice2[i]
+    NxM[i, x] = slice1[i]
+    NxM[i, y] = slice2[i]
 print(NxM)
