2ebac9e178301e3876d924872bba379f5fe02994
AnDrEUs35 <146358937+AnDrEUs35@users.noreply.github.com>
2023-11-16 16:48:41 +0000

    asjg;ajg

--- lab_4_task_1.py
+++ lab_4_task_1.py
@@ -1,13 +1,22 @@
 import numpy as np
-y = 0
+srednee = 0
+i = -1
 def sr_arifm(x):
-    for i in range(0, len(x)):
-        if y == 0:
+    for i in range(0, max(x)):
+        if srednee == 0:
             srednee = x[i]
         else:
-            srednee = srednee + x[i]    
+            srednee = srednee + x[i]  
+    return srednee
 
+srednee = sr_arifm
 
+mass = np.zeros((3))
+for i in range(0, 3):
+    mass[i] = int(input('Введите эл. массива: ')) 
 
+sr_arifm(mass)
+
+print(sr_arifm(srednee))
 
 
