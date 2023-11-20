84bf7d576346cedd71f6af73578bb273f2b15288
AnDrEUs35 <146358937+AnDrEUs35@users.noreply.github.com>
2023-11-20 14:37:50 +0000

    dgj;jgjajgdg

--- lab_4_dop_task_3.py
+++ lab_4_dop_task_3.py
@@ -1,7 +1,7 @@
 linza = input('Какая линза?: ')
 pr_lin = int(input('Введите расстояние от предмета до линзы: ')) 
 focus = int(input('фокусное расстояние:'))
-
+'''
 def focus(linza, focus, pr_lin):
     if linza == 'собирающая':
         if pr_lin > focus:
@@ -30,5 +30,32 @@ def focus(linza, focus, pr_lin):
             picture = 'мнимое, уменьшенное, прямое'
     return picture
 
-picture = focus(linza, pr_lin, focus)
+picture = focus(linza, focus, pr_lin)
+'''
+if linza == 'собирающая':
+        if pr_lin > focus:
+            if pr_lin < focus * 2:
+                picture = 'действительное, увеличенное, перевернутое'
+            elif pr_lin > focus * 2:
+                picture = 'действительное, уменьшенное, перевернутое'
+            elif pr_lin == focus * 2:
+                picture = 'действительное, реального размера, перевернутое'
+        elif pr_lin < focus:
+            picture = 'мнимое, увеличенное, прямое' 
+        elif pr_lin == focus:
+            picture = 'действительное, увеличенное, перевернутое'
+
+elif linza == 'рассеивающая':
+    if pr_lin > focus:
+        if pr_lin < focus * 2:
+            picture = 'мнимое, прямое, уменьшенное'
+        elif pr_lin > focus * 2:
+            picture = 'мнимое, прямое, уменьшенное'
+        elif pr_lin == focus * 2:
+                picture = 'мнимое, уменьшенное, прямое'
+    elif pr_lin < focus:
+        picture = 'мнимое, уменьшенное, прямое'    
+    elif pr_lin == focus:
+            picture = 'мнимое, уменьшенное, прямое'
+
 print(picture)
