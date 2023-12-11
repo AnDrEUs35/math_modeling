a128d5dbaf63f59397f6d9ad055a3fe5b37be339
AnDrEUs35 <146358937+AnDrEUs35@users.noreply.github.com>
2023-12-11 14:44:43 +0000

    jhojhoiughkj

--- lab_5_dop_task_2.py
+++ lab_5_dop_task_2.py
@@ -1,13 +1,12 @@
 import random
 
 def func(n, list0):
-    i = 0
-    rand_num = random.randint(0, n)
-    while rand_num == list0[i]:
-        i += 1
-        
-        
-        
-
-
+    while True:
+        rand_num = random.randint(0, n)
+        for i in list0:
+            if rand_num == i:
+                break
+            elif rand_num != i and i == list0[-1]:
+                return rand_num 
+            
 print(func(10, [3, 9, 1, 5]))        
