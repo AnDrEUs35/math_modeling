b2cbbe8a83e27eb20466828850bc163e38b09139
AnDrEUs35 <146358937+AnDrEUs35@users.noreply.github.com>
2023-12-16 15:57:52 +0000

    mgkjbkj

--- lab_6_task_3.py
+++ lab_6_task_3.py
@@ -5,13 +5,13 @@ def ellips(x_min, x_max, N, a = 10, b = 5):
     x = np.linspace(x_min, x_max, N)
     y = np.linspace(x_min, x_max, N)
 
-
-
     x, y = np.meshgrid(x, y)
 
     fxy = x**2 / a**2 + y**2 / b**2 - 1
 
-    plt.contour(x, y, fxy, levels=[1])
+    plt.contour(x, y, fxy, levels=[0])
     plt.axis('equal')
 
     plt.savefig('fig_lab_3.png')
+
+ellips(-50, 50, 1000)
