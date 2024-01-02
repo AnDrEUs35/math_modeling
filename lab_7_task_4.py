c60d8289e26d4367b2dec05bc135cc625377840c
AnDrEUs35 <146358937+AnDrEUs35@users.noreply.github.com>
2024-01-02 09:07:16 +0000

    hf;klfhls

--- lab_7_task_4.py
+++ lab_7_task_4.py
@@ -4,23 +4,23 @@ from matplotlib.animation import FuncAnimation
 
 
 def drawing(C, D, y0, x0, time):
-    xn = x0 * time
-    yn = y0 * time 
-    x = xn ** 2 - yn ** 2 + C
-    y = 2 * xn * yn + D
-    return x, y
+    xn1 = x0 * time
+    yn1 = y0 * time
+    xn = (xn1 ** 2 - yn1 ** 2 + C)
+    yn = (2 * xn1 * yn1 + D)
+    return xn, yn
 
 def animate(i):
     fractal.set_data(drawing(C=0.3, D=0.33, x0=0.1, y0=0.1, time=i))
 
 if __name__ == '__main__':
     fig, ax = plt.subplots()
-    fractal, = plt.plot([], [], 'o', color='r', label='ball')
+    fractal, = plt.plot([], [], 'o', color='r')
 
-    edge = 10
+    edge = 100
     plt.axis('equal')
     ax.set_xlim(-edge, edge)
     ax.set_ylim(-edge, edge)
 
-    ani = FuncAnimation(fig, animate, frames=10, interval=30)
+    ani = FuncAnimation(fig, animate, frames=180, interval=60)
     ani.save('animation_lab_4.gif')
