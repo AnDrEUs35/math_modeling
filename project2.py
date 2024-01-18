7c170a709ab97707857d4d77eaf6cb74f16fda65
AnDrEUs35 <146358937+AnDrEUs35@users.noreply.github.com>
2024-01-18 15:58:46 +0000

    fxzkcbnmcjflmscjkvlkjds

--- project2.py
+++ project2.py
@@ -2,26 +2,30 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
+# скорость звука в воздухе
+v = 343
+
 # определяем функции для рисования синей и красной волн
-def drawing1(A, f, x0):
+def drawing1(A, f, x0, v_source, v_observer):
     t = np.linspace(0, 2, 100)
-    y = A * np.sin(f * t)
+    f_obs = f * (1 + v_source/v) / (1 + v_observer/v)
+    y = A * np.sin(f_obs * t)
     x = x0 + t 
     return x, y
 
-def drawing2(A, f, x0):
+def drawing2(A, f, x0, v_source, v_observer):
     t = np.linspace(0, 2, 100)
-    y = A * np.sin(f * t)
+    f_obs = f * (1 + v_source/v) / (1 + v_observer/v)
+    y = A * np.sin(f_obs * t)
     x = x0 + -t
     return x, y
 
 def animate(phi):
-    sin1.set_data(drawing1(A=1.5, f=phi, x0=phi * 0.5))
-    sin2.set_data(drawing2(A=1.5, f=-phi, x0=phi * 0.5))
+    sin1.set_data(drawing1(A=1.5, f=phi, x0=phi * 0.5, v_source=0.5, v_observer=0.5))
+    sin2.set_data(drawing2(A=1.5, f=-phi, x0=phi * 0.5, v_source=0.5, v_observer=0.5))
 
 # создаем фигуру и оси
 fig, ax = plt.subplots()
-
 
 sin1, = plt.plot([], [], '-', color='r')
 sin2, = plt.plot([], [], '-', color='b')
