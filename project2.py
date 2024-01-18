63e567dd4b8341823d4603ad01472607477c6311
AnDrEUs35 <146358937+AnDrEUs35@users.noreply.github.com>
2024-01-18 19:31:55 +0000

    jnlknkji

--- project2.py
+++ project2.py
@@ -2,27 +2,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
-# скорость звука в воздухе
-v = 343
-
 # определяем функции для рисования синей и красной волн
-def drawing1(A, f, x0, v_source, v_observer):
+def drawing1(A, f, x0):
     t = np.linspace(0, 2, 100)
-    f_obs = f * (1 + v_source/v) / (1 + v_observer/v)
-    y = A * np.sin(f_obs * t)
+    y = A * np.sin(f * t)
     x = x0 + t 
     return x, y
 
-def drawing2(A, f, x0, v_source, v_observer):
+def drawing2(A, f, x0):
     t = np.linspace(0, 2, 100)
-    f_obs = f * (1 + v_source/v) / (1 + v_observer/v)
-    y = A * np.sin(f_obs * t)
+    y = A * np.sin(f * t)
     x = x0 + -t
     return x, y
 
-def animate(phi):
-    sin1.set_data(drawing1(A=1.5, f=phi, x0=phi * 0.5, v_source=0.5, v_observer=0.5))
-    sin2.set_data(drawing2(A=1.5, f=-phi, x0=phi * 0.5, v_source=0.5, v_observer=0.5))
+def animate(i):
+    f = 1 - (i / 20)
+    sin1.set_data(*drawing1(A=1.5, f=f, x0=f * 0.5))
+    sin2.set_data(*drawing2(A=1.5, f=-f, x0=f * 0.5))
 
 # создаем фигуру и оси
 fig, ax = plt.subplots()
@@ -36,5 +32,5 @@ ax.set_xlim(-edge, edge)
 ax.set_ylim(-edge, edge)
 
 # сохраняем анимацию в файл GIF
-ani = FuncAnimation(fig, animate, frames=np.linspace(0, 5, 70), interval=100)
-ani.save('project.gif')
+ani = FuncAnimation(fig, animate, frames=180, interval=100)
+ani.save('project_test.gif', writer='pillow')
