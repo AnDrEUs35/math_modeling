fd5bfadd69414364a1dd7cdc66c7a8dda66c368a
AnDrEUs35 <146358937+AnDrEUs35@users.noreply.github.com>
2024-01-25 16:46:58 +0000

    jssahfkjfkla

--- project.py
+++ project.py
@@ -4,13 +4,13 @@ from matplotlib.animation import FuncAnimation
 
 # определяем функции для рисования синей и красной волн
 def drawing1(A, f, x0):
-    t = np.linspace(0, 3, 100)
+    t = np.linspace(0, 5, 100)
     y = A * np.sin(f * t)
     x = x0 + t 
     return x, y
 
 def drawing2(A, f, x0):
-    t = np.linspace(0, 3, 100)
+    t = np.linspace(0, 5, 100)
     y = A * np.sin(f * t)
     x = x0 + -t
     return x, y
@@ -23,8 +23,8 @@ def circle(f, x0):
 
 # как сделать так, чтобы частота волн менялась со временем (я моделирую эффект Доплера)
 def animate(phi):
-    sin1.set_data(drawing1(A=1.5, f=phi, x0=phi * 0.5))
-    sin2.set_data(drawing2(A=1.5, f=-phi, x0=phi * 0.5))
+    sin1.set_data(drawing1(A=np.linspace(1.5, 3, 100), f=phi, x0=phi * 0.5))
+    sin2.set_data(drawing2(A=np.linspace(1.5, 0.5, 100), f=-phi, x0=phi * 0.5))
     point.set_data(circle(x0=phi * 0.5, f=np.linspace(0, 7, 70)))
 
 # создаем фигуру и оси
@@ -36,7 +36,7 @@ sin2, = plt.plot([], [], '-', color='b')
 point, = plt.plot([], [], '-', color='green')
 
 # устанавливаем пределы осей
-edge = 5
+edge = 10
 ax.set_xlim(-edge, edge)
 ax.set_ylim(-edge, edge)
 
