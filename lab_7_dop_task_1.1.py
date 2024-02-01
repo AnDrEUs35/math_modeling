2207c118197b9274e63d8e6b972921ce77cec506
AnDrEUs35 <146358937+AnDrEUs35@users.noreply.github.com>
2024-02-01 16:49:33 +0000

    kjasflhshdfhkh

--- lab_7_dop_task_1.1.py
+++ lab_7_dop_task_1.1.py
@@ -2,32 +2,49 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
-def move1(R, angle_vel):
-    t = angle_vel * np.pi
-    x = R * t - R * np.sin(t)
-    y = R - R * np.cos(t)
+def move1(R, angle_vel, time):
+    t = angle_vel * time 
+    x = R * (t - np.sin(t))
+    y = R * (1 - np.cos(t))
     return x, y
 def move2(R, time, angle_vel):
-    t = angle_vel * np.pi / 180 * time
-    x = R * t - R * np.sin(t)
-    y = R - R * np.cos(t)
+    t = angle_vel * time 
+    x = R * (t - np.sin(t))
+    y = R * (1 - np.cos(t))
+    return x, y
+def circle_move(R, vx0, time):
+    x0 = vx0 * time
+    alpha = np.arange(0, 2 * np.pi, 0.1)
+    x = x0 + R * np.cos(alpha)
+    y = R + R * np.sin(alpha)
     return x, y
 
-def animate(i):
-    ball1.set_data(move1(R=2, angle_vel=np.arange(0, 1 * np.pi, 0.01)))
-    ball2.set_data(move2(R=2, angle_vel=1, time=i))
+x, y = [], []
 
+def animate(i):
+    coords = move1(R=2, angle_vel=1, time=i)
+    x.append(coords[0])
+    y.append(coords[1])
+
+    ball1.set_data(x, y)
+    ball2.set_data(move2(R=2, angle_vel=1, time=i))
+    circle.set_data(circle_move(R=2, vx0=2, time=i))
 
 if __name__ == '__main__':
     fig, ax = plt.subplots()
-    ball1, = plt.plot([],[], 'o', color='b', lw=3)
-    ball2, = plt.plot([], [], 'o', color='r', lw=3)
+    ball1, = plt.plot([],[], '-', color='b', lw=2)
+    ball2, = plt.plot([], [], 'o', color='r', lw=2)
+    circle, = plt.plot([], [], '-', color='black')
+
+
+
+
     edge = 20
     plt.axis('equal')
     ax.set_xlim(-2, edge)
     ax.set_ylim(-5, edge)
 
-    ani = FuncAnimation(fig, animate, frames=520, interval=30)
+    ani = FuncAnimation(fig, animate, frames=np.linspace(0, 3*np.pi, 100), interval=30)
     ani.save('animation_dop_lab_1.1.gif')
 
 
--- lab_7_dop_task_3.py
+++ lab_7_dop_task_3.py
@@ -2,30 +2,28 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
-def drawing_star(t):
+def drawing_star(t, a):
     x = 12 * np.cos(t) + 8 * np.cos(1.5 * t)
     y = 12 * np.sin(t) - 8 * np.sin(1.5 * t)
-    return x, y
-def spin_star(t, x, y):
-    X = x * np.cos(t) - y * np.sin(t)
-    Y = x * np.sin(t) + y * np.cos(t)
+    
+    X = x * np.cos(a) - y * np.sin(a)
+    Y = y * np.cos(a) + x * np.sin(a)
     return X, Y
 
 
 def animate(i):
-    star.set_data(drawing_star(t=np.arange(0, 4 * np.pi, 0.001)))
-    x, y = drawing_star(t=np.arange(0, 4 * np.pi, 0.001))
-    star.set_data(spin_star, x, y, t=np.arange(0, 4 * np.pi, 0.001) * i)
+    star.set_data(drawing_star(t=np.linspace(0, 4 * np.pi, 100), a=i))
+
 if __name__ == '__main__':
     fig, ax = plt.subplots()
-    star, = plt.plot([], [], 'o', color='yellow')
+    star, = plt.plot([], [], '-', color='yellow')
 
     edge = 25
     plt.axis('equal')
     ax.set_xlim(-edge, edge)
     ax.set_ylim(-edge, edge)
 
-    ani = FuncAnimation(fig, animate, frames=180, interval=60)
+    ani = FuncAnimation(fig, animate, frames=np.linspace(0, 2 * np.pi, 100), interval=60)
     ani.save('animation_dop_lab_3.gif')
 
 
