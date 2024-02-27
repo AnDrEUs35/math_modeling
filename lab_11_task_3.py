a56c61fc424d6b4b5a65ce20bc59abc78f39aa87
AnDrEUs35 <146358937+AnDrEUs35@users.noreply.github.com>
2024-02-27 17:09:31 +0000

    jgglsjklgd

--- lab_11_task_3.py
+++ lab_11_task_3.py
@@ -4,31 +4,30 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
 frames = 200
-t = np.linspace(0, 10000, frames)
+t = np.linspace(0, 1000, frames)
 
 def move_func(z, t):
-    x, vx, y, vy = z
-    dxdt = vx
-    dvxdt = k1 * (A - x - y)
-    dydt = vx
-    dvydt = k2 * (A - x - y)
-    return dxdt, dvxdt, dydt, dvydt
+    x, y = z
+    dXdt = k1 * (A - X - Y)
+    dYdt = k2 * (A - X - Y)
+    return dXdt, dYdt
 
 k1 = 0.3
 k2 = 0.7
-A = 10
+A = 100
+X = 0
+Y = 0
+
 x0 = 0
-vx0 = 0
-vy0 = 0
 y0 = 0
 
-z0 = x0, vx0, y0, vy0
+z0 = x0, y0
 
 sol = odeint(move_func, z0, t)
 
 def solve_func(i):
     x = sol[:i, 0]
-    y = sol[:i, 2]
+    y = sol[:i, 1]
     return x, y
 
 fig, ax = plt.subplots()
@@ -36,9 +35,9 @@ ball_line, = plt.plot([], [], '-', color='r')
 
 def animate(i):
     ball_line.set_data(solve_func(i))
-ani = FuncAnimation(fig, animate, frames=frames, interval=30)
+ani = FuncAnimation(fig, animate, frames=frames, interval=100)
 
-edge = 30
+edge = 100
 ax.set_xlim(0, edge)
 ax.set_ylim(0, edge)
 
