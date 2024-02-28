d2c3ddf6eb993a9392fbd3fb9bd5edf38d819d33
AnDrEUs35 <146358937+AnDrEUs35@users.noreply.github.com>
2024-02-28 15:43:36 +0000

    m;bvmb;m;lcx

--- lab_11_task_3.py
+++ lab_11_task_3.py
@@ -4,40 +4,49 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
 frames = 200
-t = np.linspace(0, 1000, frames)
+t = np.linspace(0, 5, frames)
 
 def move_func(z, t):
-    x, y = z
-    dXdt = k1 * (A - X - Y)
-    dYdt = k2 * (A - X - Y)
-    return dXdt, dYdt
-
-k1 = 0.3
-k2 = 0.7
-A = 100
-X = 0
-Y = 0
+    x, vx, y, vy = z
+    dxdt = vx
+    dydt = vy
+    dvxdt = k1 * (A - vx - vy)
+    dvydt = k2 * (A - vx - vy)
+    return dxdt, dvxdt, dydt, dvydt
+k1 = 0.2
+k2 = 0.3
+A = 30
+v0 = 0
+alpha = 60 * np.pi / 180
 
 x0 = 0
+vx0 = v0 * np.cos(alpha)
 y0 = 0
+vy0 = v0 * np.sin(alpha)
 
-z0 = x0, y0
+z0 = x0, vx0, y0, vy0
 
 sol = odeint(move_func, z0, t)
 
-def solve_func(i):
-    x = sol[:i, 0]
-    y = sol[:i, 1]
+def solve_func(i, key):
+    if key =='point':
+        x = sol[i, 0]
+        y = sol[i, 1]
+    else:
+        x = sol[:i, 0]
+        y = sol[:i, 1]
     return x, y
 
 fig, ax = plt.subplots()
+ball, = plt.plot([], [], 'o', color='r')
 ball_line, = plt.plot([], [], '-', color='r')
 
 def animate(i):
-    ball_line.set_data(solve_func(i))
-ani = FuncAnimation(fig, animate, frames=frames, interval=100)
+    ball.set_data(solve_func(i, 'point'))
+    ball_line.set_data(solve_func(i, 'line'))
+ani = FuncAnimation(fig, animate, frames=frames, interval=30)
 
-edge = 100
+edge = 40
 ax.set_xlim(0, edge)
 ax.set_ylim(0, edge)
 
