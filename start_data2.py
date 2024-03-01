49686b153ad112dc91fe2b1c28d857a784395f87
AnDrEUs35 <146358937+AnDrEUs35@users.noreply.github.com>
2024-03-01 19:55:44 +0000

    gdydjdhfjfghj

---
+++ start_data2.py
@@ -0,0 +1,32 @@
+import numpy as np
+x01 = 20
+vx01 = -1
+y01 = 0
+vy01 = 0
+
+x02 = 0
+vx02 = 0
+y02 = 40
+vy02 = 3.9
+
+a = 45 * np.pi / 180
+x03 = 60
+vx03 = -1 * np.cos(a)
+y03 = 60
+vy03 = -1 * np.sin(a)
+
+x04 = -62
+vx04 = 4
+y04 = 0
+vy04 = 0
+
+a = 45 * np.pi / 180
+x05 = -78
+vx05 = -6 * np.cos(a)
+y05 = -78
+vy05 = -6 * np.sin(a)
+
+x06 = 0
+vx06 = 0
+y06 = -44
+vy06 = -2
